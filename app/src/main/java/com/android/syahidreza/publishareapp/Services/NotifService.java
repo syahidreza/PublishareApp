0af4b79281853445a7000ac8e61dfb330c7a294b
Syahid Reza Mutahari <syahidreza@gmail.com>
2020-07-19 22:20:55 +0700

    percobaan push notification

---
+++ app/src/main/java/com/android/syahidreza/publishareapp/Services/NotifService.java
@@ -0,0 +1,23 @@
+package com.android.syahidreza.publishareapp.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class NotifService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(remoteMessage);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
